8ec1d8f5c7d2a019687412e2b5e507e8e116d73a
Iliya.mi <iliya.mi78@gmail.com>
2021-02-03 15:30:33 +0330

    Node Class added.

---
+++ Foods_Interference/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foods_Interference
+{
+
+    class Node
+    {
+        public string Food { get; set; }
+        public List<string> Ingredients = new List<string>();
+
+    }
+}
