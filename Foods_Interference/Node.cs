b805502be67e505a739857f49b1ef9397e96a6bf
Iliya <Iliya.mi78@gmail.com>
2021-02-04 21:50:43 +0330

    Removed visited

--- Foods_Interference/Node.cs
+++ Foods_Interference/Node.cs
@@ -12,6 +12,5 @@ namespace Foods_Interference
         public string Food { get; set; }
         public int number = -1;
         public List<string> Ingredients = new List<string>();
-        bool Visited = false;
     }
 }
